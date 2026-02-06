c82478500bfa140e4ad1144ab9103e6940e0bd58
josefranciscol <46107720+josefranciscol@users.noreply.github.com>
2026-02-06 12:04:49 +0100

    Update INE_GRAFICO.js

--- INE_GRAFICO.js
+++ INE_GRAFICO.js
@@ -1,1 +1,13 @@
-<script type="text/javascript">var script = document.createElement('script');function lastNodo(obj){if (obj.children.length>0)return lastNodo(obj.children[obj.children.length-1]);else return obj;}script.setAttribute('type', 'text/javascript');script.setAttribute('src', 'https://www.ine.es/menus/plantillas/jaxiT3/js/widget.js');script.dataPost='t=50913&ratio=0.9&nocab=1&btnWidgetMapa=&p_widgetFormMapa=1&p_widget_vartipoMapa=70&p_widget_intervalos=4&p_widget_colorMapa=4&p_widget_declegend=3&rows=110924&columns=110926&columns=110925&columns=p_per&columns=p_oper&oper=25&cri110924=*&cri110925=5275938&cri110926=5275951&periodo=12~2025&declegend_selec=3&intervalos_svg=4&radioMap=70&w_contorno_col=F9F9F9&w_titulo=true&w_ancho_widget=&w_alto_widget=';script.padre=lastNodo(document.getElementsByTagName('body')[0]);script.onload = function(event){var aux=this.padre;this.padre=null;if (aux!=null)PintaWidgetMapa(aux,this.dataPost);};script.onreadystatechange = function () {if (this.readyState === 'complete' || this.readyState === 'loaded') {var aux=this.padre;this.padre=null;if (aux!=null)PintaWidgetMapa(aux,this.dataPost);}};document.head.appendChild(script);</script>
+<!DOCTYPE html>
+    <html>
+      <head>
+        <!-- Cargamos el script correctamente con la etiqueta <script> -->
+        <script type="text/javascript">var script = document.createElement('script');function lastNodo(obj){if (obj.children.length>0)return lastNodo(obj.children[obj.children.length-1]);else return obj;}script.setAttribute('type', 'text/javascript');script.setAttribute('src', 'https://www.ine.es/menus/plantillas/jaxiT3/js/widget.js');script.dataPost='t=50913&ratio=0.9&nocab=1&btnWidgetMapa=&p_widgetFormMapa=1&p_widget_vartipoMapa=70&p_widget_intervalos=4&p_widget_colorMapa=4&p_widget_declegend=3&rows=110924&columns=110926&columns=110925&columns=p_per&columns=p_oper&oper=25&cri110924=*&cri110925=5275938&cri110926=5275951&periodo=12~2025&declegend_selec=3&intervalos_svg=4&radioMap=70&w_contorno_col=F9F9F9&w_titulo=true&w_ancho_widget=&w_alto_widget=';script.padre=lastNodo(document.getElementsByTagName('body')[0]);script.onload = function(event){var aux=this.padre;this.padre=null;if (aux!=null)PintaWidgetMapa(aux,this.dataPost);};script.onreadystatechange = function () {if (this.readyState === 'complete' || this.readyState === 'loaded') {var aux=this.padre;this.padre=null;if (aux!=null)PintaWidgetMapa(aux,this.dataPost);}};document.head.appendChild(script);</script>
+      </head>
+      <body>
+        <!-- Contenedor donde el script dibujará el gráfico -->
+        <div id="grafico"></div>
+      </body>
+    </html>
+
+  
